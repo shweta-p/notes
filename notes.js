83ea03870a9aad681f58ad82cb1e602eb8e98070
Shweta Panditrao <panditraos@yahoo-inc.com>
2015-10-29 20:38:34 -0700

    updates

--- notes.js
+++ notes.js
@@ -7,6 +7,18 @@
     var filterFormElements = $('#notesOrder, #notesOrderDirection');
     var notesSearchFilter = $('#notesSearchFilter');
 
+    var activeNoteId = null;
+
+    function resizeTextArea(textarea) {
+      textarea.css('overflowY', 'hidden');
+      textarea.height('initial');
+      textarea.height(Math.min(textarea.prop('scrollHeight'), 214) + 'px');
+    }
+
+    $(document).on('keyup', '.note-content', function (e) {
+      resizeTextArea($(e.target));
+    });
+
     // Constructor for a note
     function Note (noteText, createdAt, updatedAt) {
       this.noteText = noteText;
@@ -30,7 +42,7 @@
 
     // Pretty-fies a javascript date object
     function prettyDate(date) {
-      return moment(date).fromNow();
+      return moment(date).calendar();
     }
 
     // Adds a note to html page
@@ -38,7 +50,7 @@
       var updatedDate = prettyDate(note.updatedAt);
       var createdDate = prettyDate(note.createdAt);
       var notePreview = note.noteText.split('\n')[0];
-      notesContainer.prepend('<div class="note" id="' + note.id + '"><div class="note-preview">' + notePreview + '</div><div class="dates"><span class="updated-date">' + updatedDate +  '</span><span class="created-date">' + createdDate + '</span></div><textarea class="note-content" disabled>' + note.noteText + '</textarea>' + '<button class="edit-note">Edit</button><button class="delete-note">Delete</button></div>');
+      notesContainer.prepend('<div class="note edit-note" id="' + note.id + '"><div class="note-preview"><button class="delete-note note-actions" title="Delete"><span class="glyphicon glyphicon-trash" aria-label="Edit"></span></button><div class="note-title">' + notePreview + '</div></div><div class="dates"><span class="updated-date">' + updatedDate +  '</span><span class="created-date">' + createdDate + '</span></div><textarea class="note-content" rows="2" disabled>' + note.noteText + '</textarea>' + '</div>');
     }
 
     // Saves all notes to local storage
@@ -63,8 +75,17 @@
     // Rerenders all notes in the html page
     function renderNotes(notes) {
       notesContainer.empty();
-      var asc = $('#notesOrderDirection').val() === "asc";
+      var asc;
+
       var orderBy = $('#notesOrder').val() === "created" ? 'createdAt' : 'updatedAt';
+
+      // This is statement only applies if the "newest to oldest" filtering feature is turned on
+      if ($('#notesOrderDirection').val()) {
+        asc = $('#notesOrderDirection').val() === "asc";
+      } else {
+        asc = orderBy === 'updatedAt'; 
+      }
+
       var searchTerm = notesSearchFilter.val().toLowerCase();
 
       var filteredNotes = notes.filter(function (note) {
@@ -72,7 +93,7 @@
       });
 
       var filteredNotes = filteredNotes.sort(function (note1, note2) {
-        return asc ? note1[orderBy] < note2[orderBy] : note1[orderBy] > note2[orderBy];
+        return asc ? note1[orderBy] > note2[orderBy] : note1[orderBy] < note2[orderBy];
       });
 
       var i;
@@ -90,39 +111,39 @@
       addNote(note);
       saveNotes();
       newNoteContent.val(null);
+      resizeTextArea(newNoteContent);
     });
 
     // Handles the edit note action by enabling the editing of a note and displaying the save button
     notesContainer.on('click', '.edit-note', function(e) {
-      var note = $(e.target).parent();
-      note.find('.note-content').prop('disabled', false);
-      $(e.target).removeClass('edit-note');
-      $(e.target).addClass('save-note');
-    });
-
-    // Handles the save note action by saving the newly edited not
-    notesContainer.on('click', '.save-note', function(e) {
-      var note = $(e.target).parent();
-      var id = note.attr('id');
-      var i, updatedDate;
-      var noteContent = note.find('.note-content').val();
-      for (i = 0; i < notes.length; i++) {
-        if (id === notes[i].id) {
-          notes[i].noteText = noteContent;
-          updatedDate = notes[i].update();
+      var note = $(e.target).closest('.note');
+      var noteId = note.attr('id');
+      if (activeNoteId) {
+        // close and save the currently open note
+        $('#' + activeNoteId).find('.note-content').prop('disabled', true);
+        var activeNoteContent = $('#' + activeNoteId).find('.note-content').val();
+        for (i = 0; i < notes.length; i++) {
+          if (activeNoteId === notes[i].id) {
+            notes[i].noteText = activeNoteContent;
+            updatedDate = notes[i].update();
+          }
         }
+        $('#' + activeNoteId).find('.note-title').text(activeNoteContent.split('\n')[0]);
+        saveNotes();
       }
-      note.find('.note-content').prop('disabled', true);
-      note.find('.updated-date').text(prettyDate(updatedDate));
-      note.find('.note-preview').text(noteContent.split('\n')[0]);
-      $(e.target).addClass('edit-note');
-      $(e.target).removeClass('save-note');
-      saveNotes();
+      // if user clicks on a note header for a note that's already open then we close that note
+      if (activeNoteId != noteId || $(e.target).prop('tagName').toLowerCase() === 'textarea') {
+        activeNoteId = noteId;
+        note.find('.note-content').prop('disabled', false);
+        resizeTextArea(note.find('.note-content'));
+      } else {
+        activeNoteId = null;
+      }
     });
 
     // Deletes a node; TODO: "are you sure you wanna delete?"
     notesContainer.on('click', '.delete-note', function(e) {
-      var note = $(e.target).parent();
+      var note = $(e.target).closest('.note');
       notes = notes.filter(function (aNote) {
         return aNote.id != note.attr('id');
       });
